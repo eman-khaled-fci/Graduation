59caf7e2aba025923fc2cba37240cbf0e5bdb5df
eman-khaled-fci <mony.5aled.fci@gmail.com>
2022-12-04 20:27:05 +0200

    forth commit

---
+++ lib/components/already_have_account.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+import '../screens/sign_in/sign_in_screen.dart';
+import '../screens/sign_up/sign_up_screen.dart';
+import '../size_config.dart';
+
+class AlreadyHaveAccount extends StatelessWidget {
+  const AlreadyHaveAccount({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("Already have an account?",style: LightModeSmallTextStyle),
+        GestureDetector(
+            onTap:() => Navigator.pushNamed(context, SignInScreen.routeName),
+            child:
+            Text("Log in ",style: TextStyle(fontSize: getProportionateScreenWidth(16),color: LightModeMainColor),)),
+
+      ],
+    );
+  }
+}
